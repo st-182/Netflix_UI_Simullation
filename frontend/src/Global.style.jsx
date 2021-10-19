4d0626eff17497b8495248a3e03fcb3da88ebf89
st-182 <31894656+st-182@users.noreply.github.com>
2021-10-19 12:24:53 +0300

    +global styles

--- frontend/src/Global.style.jsx
+++ frontend/src/Global.style.jsx
@@ -2,5 +2,10 @@ import styled, { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 /* style  */
+body{
+    background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("https://assets.nflxext.com/ffe/siteui/vlv3/68c9706b-acd1-4472-bb1d-ef3ca933154c/b72e13a3-4bc9-404e-a2cd-ac1600e9851d/LT-en-20211011-popsignuptwoweeks-perspective_alpha_website_small.jpg") no-repeat  ;
+    background-position: 100%;
+    min-height: 100vh;
+}
 `;
 export default GlobalStyle;
